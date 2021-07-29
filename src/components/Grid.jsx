dfe121e08b81dcd498ecfdaa21dd1918409a1b47
dandelionadia <nadia.ridko@gmail.com>
2021-07-29 16:37:33 +0200

    adds Grid component

---
+++ src/components/Grid.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Box } from "atomic-layout"
+import styled from "styled-components"
+
+const StyledBox = styled.div`
+  box-sizing: border-box;
+`
+
+export const Grid = ({ children, fluid }) => {
+  const maxWidths = fluid
+    ? { maxWidth: "1400px" }
+    : { maxWidthSm: "540px", maxWidthMd: "1200px" }
+
+  return (
+    <Box
+      as={StyledBox}
+      width="100%"
+      {...maxWidths}
+      marginHorizontal="auto"
+      paddingHorizontal={2.6}
+      paddingHorizontalLg={8}
+    >
+      {children}
+    </Box>
+  )
+}
