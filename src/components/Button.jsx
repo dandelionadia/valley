1d51710ce440bf3cc4ca67d25d7c89e98eaf97ce
dandelionadia <nadia.ridko@gmail.com>
2021-08-03 13:39:10 +0200

    Button: adds styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,10 +5,11 @@ export const Button = styled.button`
   color: ${props => (props.primary ? "#f7fafc" : "alevioletred")};
 
   font-size: 14px;
+  font-weight: var(--fontWeight-extrabold);
   margin: 1em;
   padding: 0.7rem 2em;
   border: none;
-  border-radius: 1px;
   text-transform: uppercase;
+  letter-spacing: var(--letter-spacing-2);
   box-shadow: 8px 8px 1px 1px rgb(33, 40, 84);
 `
