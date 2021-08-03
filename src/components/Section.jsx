e0fd1db41bd39f7a6032fb880d8b190157b53dcc
dandelionadia <nadia.ridko@gmail.com>
2021-08-03 13:59:28 +0200

    Section: fixes paddingVertical

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,17 +1,21 @@
 import * as React from "react"
+import { Box } from "atomic-layout"
 import styled from "styled-components"
 
 import { Grid } from "../components/Grid"
 
-const Container = styled.div`
+const Container = styled(Box)`
   background-color: var(--color-heading);
   color: var(--color-gray-light);
-  padding: var(--spacing-24);
 `
 
 const Section = ({ children }) => {
   return (
-    <Container>
+    <Container
+      paddingVertical="var(--spacing-12)"
+      paddingVerticalMd="var(--spacing-20)"
+      paddingVerticalLg="var(--spacing-24)"
+    >
       <Grid>{children}</Grid>
     </Container>
   )
