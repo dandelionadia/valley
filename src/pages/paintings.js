a0e9cb791aa2aebb73289b00e8329bf27c691841
dandelionadia <nadia.ridko@gmail.com>
2021-08-05 18:28:16 +0200

    paintings: images: adds grid responsive

--- src/pages/paintings.js
+++ src/pages/paintings.js
@@ -1,18 +1,51 @@
 import * as React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
+
 import { DefaultLayout } from "../layouts/DefaultLayout"
 import PaintingsLayout from "../layouts/PaintingsLayout"
+
+const StyledGallery = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-rows: masonry;
+  gap: var(--spacing-5);
+  grid-auto-flow: dense;
+`
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  ${props =>
+    props.imageIndex &&
+    `
+      grid-column: span 2;
+      grid-row: span 2;
+
+      @media (max-width: 768px) {
+        grid-column: span 1;
+      }
+    `}
+`
 
 const paintings = ({ data }) => {
   console.log(data)
   return (
     <DefaultLayout>
       <PaintingsLayout>
-        <div>
-          {data.paintings.nodes.map(painting => {
-            return <img src={painting.frontmatter.imageUrl} />
+        <StyledGallery>
+          {data.paintings.nodes.map((painting, index) => {
+            const imageIndex = (index + 1) % 3 === 0
+            return (
+              <StyledImage
+                src={painting.frontmatter.imageUrl}
+                imageIndex={imageIndex}
+              />
+            )
           })}
-        </div>
+        </StyledGallery>
       </PaintingsLayout>
     </DefaultLayout>
   )
