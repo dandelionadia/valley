c5e4d6a3810bd64db87d3495dec36198555112eb
dandelionadia <nadia.ridko@gmail.com>
2021-08-11 15:46:44 +0200

    wip

--- src/components/ArtGallery.jsx
+++ src/components/ArtGallery.jsx
@@ -26,6 +26,28 @@ const StyledLi = styled.li`
       color: var(--color-light-blue);
       border-bottom: 1px solid var(--color-light-blue);
     }
+
+    .active {
+      color: var(--color-light-blue);
+      border-bottom: 1px solid var(--color-light-blue);
+    }
+  }
+`
+
+const StyledTitle = styled.h3`
+  padding-left: 16px;
+
+  @media (max-width: 992px) {
+    text-align: center;
+    padding: 0;
+  }
+`
+
+const Container = styled(Box)`
+  @media (min-width: 992px) {
+    position: sticky;
+    top: 100px;
+    height: fit-content;
   }
 `
 
@@ -34,14 +56,14 @@ const ArtGallery = ({ children }) => {
     <Section>
       <Composition
         paddingHorizontal="var(--spacing-12)"
-        templateCols="auto auto"
-        gap="var(--spacing-24)"
+        templateCols="auto"
+        templateColsLg="auto auto"
+        gap="var(--spacing-6)"
+        gapLg="var(--spacing-12)"
       >
-        <Box>
-          <Box as="h3" paddingLeft="16px">
-            gallery
-          </Box>
-          <StyledUl>
+        <Container minWidth="200px">
+          <StyledTitle as="h3">gallery</StyledTitle>
+          <StyledUl activeClassName="active">
             <StyledLi>
               <Link to={"/paintings/concept"} itemProp="url">
                 <div itemProp="headline">Concept art</div>
@@ -68,7 +90,7 @@ const ArtGallery = ({ children }) => {
               </Link>
             </StyledLi>
           </StyledUl>
-        </Box>
+        </Container>
         <Box>{children}</Box>
       </Composition>
     </Section>
